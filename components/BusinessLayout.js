123083bb7a7f8569037cb5ea49c4f389d3a8e6cc
DevWithEasy <devwitheasy@gmail.com>
2024-02-03 19:44:53 +0600

    bussiness layout add

--- components/BusinessLayout.js
+++ components/BusinessLayout.js
@@ -1,9 +1,69 @@
 import React from 'react';
 
-const BusinessLayout = () => {
+const BusinessLayout = ({children}) => {
+    const sidebars = [
+        {
+            title : 'Business Team',
+            desc : 'Add, remove or change role',
+            path : 'team',
+        },
+        {
+            title : 'Business Profile',
+            desc : 'Edit business details',
+            path : 'profile',
+        },
+        {
+            title : 'Settings',
+            desc : 'Change owner or delete business',
+            path : 'settings',
+        },
+    ]
     return (
-        <div>
-            
+        <div
+            className=''
+        >
+            <div
+                className='h-[70px] px-6 border-b flex items-center'
+            >
+                <p
+                    className='text-2xl'
+                >
+                    Business Settings
+                </p>
+                <p className='mx-2'>(Name)</p>
+            </div>
+            <div
+                className='h-[calc(100vh-118px)] flex justify-between'
+            >
+                <div
+                    className='w-3/12 pl-4 border-r'
+                >
+                    {
+                        sidebars.map((topic,i)=>
+                        <div
+                            key={i}
+                            className='py-3 pr-3 border-b'
+                        >
+                            <div
+                                className='p-2 bg-[#EBEEFB] hover:bg-gray-100 rounded cursor-pointer'
+                            >
+                            <p>{topic.title}</p>
+                            <p
+                                className='text-sm text-gray-500'
+                            >
+                            {topic.desc}
+                            </p>
+                            </div>
+                        </div>
+                        )
+                    }
+                </div>
+                <div
+                    className='w-9/12 px-6 py-4'
+                > 
+                    {children}
+                </div>
+            </div>
         </div>
     );
 };
--- pages/business/[id].js
+++ pages/business/[id].js
@@ -1,11 +1,16 @@
 import React from 'react';
 import UserLayout from '../../components/UserLayout';
 import BussinesNames from '../../components/business/BussinesNames';
+import BusinessLayout from '../../components/BusinessLayout';
+import Team from '../../components/business/Team';
 
 const Bussiness = () => {
     return (
         <UserLayout>
             <BussinesNames/>
+            <BusinessLayout>
+                <Team/>
+            </BusinessLayout>
         </UserLayout>
     );
 };
