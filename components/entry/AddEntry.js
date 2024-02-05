50fe16e696c336bc1686b06e0385969b637cf197
DevWithEasy <devwitheasy@gmail.com>
2024-02-05 23:34:14 +0600

    code refractor

--- components/entry/AddEntry.js
+++ components/entry/AddEntry.js
@@ -6,6 +6,9 @@ import {
 import React, { useRef, useState } from 'react';
 import { BsClock } from "react-icons/bs";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
+import { IoSettingsOutline } from "react-icons/io5";
+import { RiArrowDownSFill } from "react-icons/ri";
+
 
 const AddEntry = ({ type, setType, view, setView }) => {
     const [timeView, setTimeView] = useState(false)
@@ -57,11 +60,19 @@ const AddEntry = ({ type, setType, view, setView }) => {
                         <div
                             className='flex justify-between items-center'
                         >
-                            <input
-                                type="date"
-                                className='px-4 py-2 rounded border'
-                            />
-                            <div>
+                            <div
+                                className='space-y-2'
+                            >
+                                <label className='block text-sm'>Date</label>
+                                <input
+                                    type="date"
+                                    className='px-4 py-2 rounded border focus:ring-2 ring-[#4863D4] focus:outline-none'
+                                />
+                            </div>
+                            <div
+                                className='space-y-1'
+                            >
+                                <label className='block text-sm'>Time</label>
                                 {!timeView ?
                                     <button
                                         onClick={() => setTimeView(!timeView)}
@@ -75,7 +86,7 @@ const AddEntry = ({ type, setType, view, setView }) => {
                                         className='space-x-2'
                                     >
                                         <select
-                                            className='px-4 py-2 border rounded'
+                                            className='px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
                                         >
                                             {
                                                 new Array(12).fill(0).map((_, i) =>
@@ -93,7 +104,7 @@ const AddEntry = ({ type, setType, view, setView }) => {
                                             }
                                         </select>
                                         <select
-                                            className='px-4 py-2 border rounded'
+                                            className='px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
                                         >
                                             {
                                                 new Array(59).fill(0).map((_, i) =>
@@ -111,7 +122,7 @@ const AddEntry = ({ type, setType, view, setView }) => {
                                             }
                                         </select>
                                         <select
-                                            className='px-4 py-2 border rounded'
+                                            className='px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
                                         >
                                             <option
                                                 className='flex items-center space-x-2'
@@ -135,6 +146,65 @@ const AddEntry = ({ type, setType, view, setView }) => {
                                     </div>
                                 }
 
+                            </div>
+                        </div>
+                        <div
+                            className='space-y-1'
+                        >
+                            <label className='block text-sm'>Amount</label>
+                            <input
+                                type='number'
+                                placeholder='eg - 1000'
+                                className='w-full px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
+                            />
+                        </div>
+                        <div
+                            className='space-y-1'
+                        >
+                            <label className='block text-sm'>Remark</label>
+                            <input
+                                type='text'
+                                placeholder='eg - Enter Detail (Name, Bill No, Item, Quantity etc)'
+                                className='w-full px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
+                            />
+                        </div>
+                        <div
+                            className='flex justify-between space-x-5'
+                        >
+                            <div
+                                className='w-1/2 space-y-1'
+                            >
+                                <div
+                                    className='flex justify-between items-center'
+                                >
+                                    <span className='text-sm'>Category</span>
+                                    <IoSettingsOutline
+                                        size={15}
+                                        className='text-[#4863D4] cursor-pointer'
+                                    />
+                                </div>
+                                <div
+                                    className='relative rounded'
+                                >
+                                    <input
+                                        placeholder='Search or Select' 
+                                        className='w-full px-4 py-2 border rounded focus:ring-2 ring-[#4863D4] focus:outline-none'
+                                    />
+                                    <button
+                                        className='absolute right-0 px-3 py-2.5 border-l'
+                                    >
+                                        <RiArrowDownSFill size={20}/>
+                                    </button>
+
+                                    <div
+                                        className='absolute w-full h-[100px] border rounded'
+                                    >
+                                        <p>dadgas</p>
+                                        <p>dadgas</p>
+                                        <p>dadgas</p>
+                                        <p>dadgas</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
