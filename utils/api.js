755ee6418cd8419d30789c5d353b2ef82e58e3c3
DevWithEasy <devwitheasy@gmail.com>
2024-03-09 16:23:09 +0600

    api rul change

--- utils/api.js
+++ utils/api.js
@@ -1,3 +1,3 @@
-const api = process.env.NODE_ENV === 'Production' ?  'https://cashbook-in-api.onrender.com/api'  : 'http://localhost:8080/api'
+const api = process.env.NODE_ENV === 'production' ?  'https://cashbook-in-api.onrender.com/api'  : 'http://localhost:8080/api'
 
 export default api;
